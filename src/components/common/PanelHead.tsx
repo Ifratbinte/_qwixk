ed98d9de49e66d74b63a0dff6983ea949decef24
Ifratbinte <ifratbinte@gmail.com>
2023-09-05 17:00:08 +0600

    feat[*] : panel head component create

---
+++ src/components/common/PanelHead.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import panel_action, {
+  ActionInterface,
+} from "#/components/instance/icon/svgIcon-with-text";
+import SvgText from "./widgets/svgIcon-with-text";
+
+interface PanelHeadInterface {
+  title: string;
+}
+
+const PanelHead: React.FC<PanelHeadInterface> = ({ title }) => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="text-xl">{title}</div>
+      <ul className="flex gap-6">
+        {panel_action.map((action: ActionInterface) => (
+          <SvgText title={action.content} Icon={action.Icon} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PanelHead;
